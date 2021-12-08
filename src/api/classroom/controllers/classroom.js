635ae1874a7b623658751f0415cab6c2539434d2
Khalid Elshafie <abolkog@gmail.com>
2021-12-08 22:52:05 +1100

    Chapter 2

---
+++ src/api/classroom/controllers/classroom.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ *  classroom controller
+ */
+
+const { createCoreController } = require("@strapi/strapi").factories;
+
+module.exports = createCoreController("api::classroom.classroom");
---
+++ src/api/classroom/routes/classroom.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/**
+ * classroom router.
+ */
+
+const { createCoreRouter } = require("@strapi/strapi").factories;
+
+module.exports = createCoreRouter("api::classroom.classroom");
