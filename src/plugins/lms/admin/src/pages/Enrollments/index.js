35bc53e72e5651a0d56940cd564ceefd25162427
Khalid Elshafie <abolkog@gmail.com>
2021-12-25 22:03:21 +1100

    Design system

--- src/plugins/lms/admin/src/pages/Enrollments/index.js
+++ src/plugins/lms/admin/src/pages/Enrollments/index.js
@@ -1,6 +1,9 @@
 import React, { memo, useEffect, useState } from 'react';
-
 import axios from 'axios';
+import { Box } from '@strapi/design-system/Box';
+import { Divider } from '@strapi/design-system/Divider';
+import { Typography } from '@strapi/design-system/Typography';
+import { Table, Thead, Tbody, Tr, Td, Th } from '@strapi/design-system/Table';
 
 const Enrollments = () => {
   const [classes, setClasses] = useState([]);
@@ -27,43 +30,58 @@ const Enrollments = () => {
   }, []);
 
   return (
-    <div>
-      <h1>List of classes with enrollments</h1>
+    <Box padding={10}>
+      <Typography variant="alpha">List of classes with enrollments</Typography>
+      <Divider />
+
       {classes.map(item => {
         const { id, attributes } = item;
         const students = attributes.students.data;
 
         return (
-          <React.Fragment key={id}>
-            <div>
-              <h2>{attributes.name}</h2>
-              <p>{attributes.description}</p>
-              <br />
-              <table>
-                <thead>
-                  <tr>
-                    <th>#</th>
-                    <th>name</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-
-                {students.map((student, i) => {
-                  return (
-                    <tr key={student.id}>
-                      <td>{i + 1}</td>
-                      <td>{student.attributes.username}</td>
-                      <td>{student.attributes.email}</td>
-                    </tr>
-                  );
-                })}
-              </table>
-            </div>
-            <hr />
-          </React.Fragment>
+          <Box padding={5} key={id}>
+            <Typography variant="beta">{attributes.name}</Typography>
+            <h2></h2>
+            <Typography>{attributes.description}</Typography>
+            <Box paddingTop={5}>
+              <Table>
+                <Thead>
+                  <Tr>
+                    <Th>
+                      <Typography>#</Typography>
+                    </Th>
+                    <Th>
+                      <Typography>Name</Typography>
+                    </Th>
+                    <Th>
+                      <Typography>Email</Typography>
+                    </Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {students.map((student, i) => {
+                    return (
+                      <Tr key={student.id}>
+                        <Td>
+                          <Typography>{i + 1}</Typography>
+                        </Td>
+                        <Td>
+                          <Typography>{student.attributes.username}</Typography>
+                        </Td>
+                        <Td>
+                          <Typography>{student.attributes.email}</Typography>
+                        </Td>
+                      </Tr>
+                    );
+                  })}
+                </Tbody>
+              </Table>
+            </Box>
+            <Divider />
+          </Box>
         );
       })}
-    </div>
+    </Box>
   );
 };
 
--- src/plugins/lms/admin/src/pages/HomePage/index.js
+++ src/plugins/lms/admin/src/pages/HomePage/index.js
@@ -5,18 +5,29 @@
  */
 
 import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
+import { Box } from '@strapi/design-system/Box';
+import { Typography } from '@strapi/design-system/Typography';
+import { Table, Thead, Tbody, Tr, Td, Th } from '@strapi/design-system/Table';
+
 import { Link } from 'react-router-dom';
 import pluginId from '../../pluginId';
 import pluginPkg from '../../../../package.json';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>{pluginPkg.strapi.name}</h1>
-      <h2>{pluginPkg.strapi.description}</h2>
-      <Link to={`/plugins/${pluginId}/enrollments`}>Show all Enrollments</Link>
-    </div>
+    <Box padding={10}>
+      <Box paddingTop={2}>
+        <Typography variant="alpha">{pluginPkg.strapi.name}</Typography>
+      </Box>
+      <Box paddingTop={2}>
+        <Typography>{pluginPkg.strapi.description}</Typography>
+      </Box>
+      <Box paddingTop={2}>
+        <Link to={`/plugins/${pluginId}/enrollments`}>
+          Show all Enrollments
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
