e6b36ef86bac9b28c93ec75c36805244ff4678ba
Khalid Elshafie <abolkog@gmail.com>
2021-12-27 11:45:26 +1100

    Chapter 9

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -13,4 +13,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+      },
+    },
+  },
 });
--- src/index.js
+++ src/index.js
@@ -1,5 +1,74 @@
 'use strict';
 
+/**
+ * Create role if it does not exist in the system
+ * @param {*} name The role name
+ * @param {*} type The role type
+ * @param {*} description The role description
+ */
+const createRoleIfNotExist = async (name, type, description = '') => {
+  try {
+    const role = await strapi.db
+      .query('plugin::users-permissions.role')
+      .findOne({
+        where: { type },
+      });
+
+    if (!role) {
+      await strapi.db.query('plugin::users-permissions.role').create({
+        data: {
+          name,
+          type,
+          description,
+        },
+      });
+      strapi.log.info(`Created role ${name}`);
+    }
+  } catch (e) {
+    strapi.log.error(e);
+  }
+};
+
+/** 
+
+* Enable action on a controller for a specific role 
+* @param {*} type The role type 
+* @param {*} apiName The name of the API where the controller action lives 
+* @param {*} controller The controller where the action lives 
+* @param {*} action The action itself 
+*/
+const enablePermission = async (type, apiName, controller, action) => {
+  try {
+    // Get the role entity
+    const role = await strapi.db
+      .query('plugin::users-permissions.role')
+      .findOne({
+        where: { type },
+        populate: ['permissions'],
+      });
+
+    const actionId = `api::${apiName}.${controller}.${action}`;
+
+    // Get the permissions associated with the role
+    const rolePermission = role.permissions.find(
+      permission => permission.action === actionId
+    );
+
+    if (!rolePermission) {
+      await strapi.db.query('plugin::users-permissions.permission').create({
+        data: {
+          action: actionId,
+          role: role.id,
+        },
+      });
+    }
+  } catch (e) {
+    strapi.log.error(
+      `Bootstrap script: Could not update settings. ${controller} -   ${action}`
+    );
+  }
+};
+
 module.exports = {
   /**
    * An asynchronous register function that runs before
@@ -16,5 +85,19 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  async bootstrap(/*{ strapi }*/) {
+    // Creating roles
+    await createRoleIfNotExist('Student', 'student', 'Student role');
+    await createRoleIfNotExist('Teacher', 'teacher', 'Teacher role');
+    await createRoleIfNotExist('Admin', 'admin', 'App admin role');
+
+    // Admin Role Classroom permissions
+    await enablePermission('admin', 'classroom', 'classroom', 'create');
+    await enablePermission('admin', 'classroom', 'classroom', 'find');
+    await enablePermission('admin', 'classroom', 'classroom', 'findOne');
+    await enablePermission('admin', 'classroom', 'classroom', 'findTutorials');
+    await enablePermission('admin', 'classroom', 'classroom', 'create');
+    await enablePermission('admin', 'classroom', 'classroom', 'update');
+    await enablePermission('admin', 'classroom', 'classroom', 'delete');
+  },
 };
